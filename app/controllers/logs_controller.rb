c680b0b0047028bd7b95bdd7c5d7972a3e1f7164
Matheus Nunes <matheus.o.nunes@gmail.com>
2018-01-13 23:39:45 -0200

    Fixing method for capturing request HTTP method

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -5,7 +5,7 @@ class LogsController < ApplicationController
     parser = Log::Parser.new(request.raw_post)
 
     Log.create({
-      http_method: parser.http_method,
+      http_method: parser.method,
       request_id: parser.request_id,
       status: parser.status,
       host: parser.host,
