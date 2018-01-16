33e294007b1cfab7c9119b51f6a793696d0514df
Matheus Nunes <matheus.o.nunes@gmail.com>
2018-01-16 19:36:54 -0200

    Parse multiple logs in one request individually

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -2,21 +2,23 @@ class LogsController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def create
-    parser = Log::Parser.new(request.raw_post.split("\n"))
+    request.raw_post.split("\n").each do |raw_data|
+      parser = Log::Parser.new(raw_data)
 
-    if parser.try(:request_id)
-      Log.create({
-        http_method: parser.method,
-        request_id: parser.request_id,
-        status: parser.status,
-        host: parser.host,
-        path: parser.path,
-        fwd: parser.fwd,
-        timestamp: parser.timestamp,
-        raw: request.raw_post
-      })
-    else
-      Log.create raw: request.raw_post.force_encoding('UTF-8'), timestamp: parser.timestamp
+      if parser.try(:request_id)
+        Log.create({
+          http_method: parser.method,
+          request_id: parser.request_id,
+          status: parser.status,
+          host: parser.host,
+          path: parser.path,
+          fwd: parser.fwd,
+          timestamp: parser.timestamp,
+          raw: raw_data
+        })
+      else
+        Log.create raw: raw_data.force_encoding('UTF-8'), timestamp: parser.timestamp
+      end
     end
 
     head :ok
